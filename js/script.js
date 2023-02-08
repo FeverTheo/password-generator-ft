785b34596f9afe8b26309f805573e6e78ac0e8ae
FeverTheo <tete.fever51@gmail.com>
2023-02-08 16:04:51 +0100

    generator en cours

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var takepassword = document.getElementById("mdp");
+// Fonction
+function generatePassword() {
+    var password='';
+    var caracteres='ABCDEFGHIJKLMNOPQRSTUVWXYZ'+'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    for (var i = 0; i < 12; i++) {
+        var char = Math.floor(Math.random()*caracteres.length + 1);
+        password += caracteres.charAt(char);
+    }
+    return password;
+}
+function geneOn() {
+    takepassword.innerHTML = generatePassword();
+}
